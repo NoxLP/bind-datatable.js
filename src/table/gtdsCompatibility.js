8c3656bb8b34e061efb9d5996ed36afe77aa7755
Norber <nox_nox_@hotmail.com>
2022-07-20 12:11:03 +0100

    Remove unused functions

--- src/table/gtdsCompatibility.js
+++ src/table/gtdsCompatibility.js
@@ -1,32 +1,5 @@
 import { Error } from '../error.js'
 
-export const gtds_replaceIndexId = (indexesById, index, newId) => {
-  if (typeof newId != 'string' && typeof newId != 'number')
-    Error("Updated id to a value that wasn't a string nor a number")
-
-  const id = indexesById.byIndexes[index]
-  newId = `${newId}`
-  delete indexesById.byIds[id]
-  delete indexesById.byIndexes[index]
-  indexesById.byIds[newId] = index
-  indexesById.byIndexes[index] = newId
-}
-
-export const gtds_pushIndexId = (indexesById, current, config, value) => {
-  if (!(`${config.id}` in value))
-    Error(`New row needs a key with name ${config.id}`)
-  if (`${value.id}` in indexesById.byIds)
-    Error('Registry with same id already exists')
-  indexesById.byIds[value.id] = current.length
-  indexesById.byIndexes[current.length] = value.id
-}
-
-export const gtds_removeIndexId = (indexesById, index) => {
-  const id = indexesById.byIndexes[index]
-  delete indexesById.byIds[id]
-  delete indexesById.byIndexes[index]
-}
-
 export const gtds_getDataByPrimaryKey = (current, indexesById, id) =>
   current[indexesById[id]]
 
